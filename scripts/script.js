62fd7eefc4dff1ea84ff681f007c937ab0eac15c
Emil Seyidov <rashad3974@gmail.com>
2016-09-16 13:22:15 +0000

    first commit

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,7 @@ $(function() {
   
   var colors = ['#FFFFFF', '#FFCCBF', '#FEBACE'];
   var random = Math.floor(Math.random()*colors.length);
-  $('.main').addClass('red');
+ 
   $('button').on('click', function() {
       $('.page').css('background-color', colors[random]);
   });
