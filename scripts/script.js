3fbae407ea9bc2f6062b204ee078ff1ad31bc979
Emil Seyidov <rashad3974@gmail.com>
2016-09-17 18:59:45 +0000

    first commit

--- scripts/script.js
+++ scripts/script.js
@@ -4,15 +4,20 @@
 
 $(function() {
   $("body").hide().fadeIn(2000);
-  
-  var colors = ['#FFFFFF', '#FFCCBF', '#FEBACE'];
-  var random = Math.floor(Math.random()*colors.length);
- 
-  $('button').on('click', function() {
-      $('.page').css('background-color', colors[random]);
-  });
-  $('.square5').on('click', function(){
-    $('.square5').fadeOut('fast');
-  })
-});
 
+  var colors = ['red', 'blue', 'green'];
+  var random = Math.floor(Math.random() * colors.length);
+
+  $('button').on('click', function color() {
+      $('.page').css({ 'background-color': colors[random] });
+       var button = document.getElementsByTagName('button');
+        button.queue(function color() {
+          
+        });
+
+  });
+  $('.square5').on('click', function deletion() {
+    $(".square5").remove();
+  });
+
+});
