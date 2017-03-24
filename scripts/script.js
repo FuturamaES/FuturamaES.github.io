d220aa90a5aae9ac8713ef8a9f77cd586108625e
Emil Seyidov <rashad3974@gmail.com>
2017-03-24 06:07:28 +0000

    first commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,7 @@
 /* global $*/
-/* global a */
-/* global on */
+
 
 $(function() {
  //  $("body").hide().fadeIn(5000); Makes page fade in within 5 seconds
- 
+ // alert("Welcome to this website");
 });
